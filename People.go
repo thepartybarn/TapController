64998309c881b57e4e658ae936d7449950c8d65b
Mike <schmidty1913@gmail.com>
2019-07-19 13:33:57 -0500

    added sql logic

--- People.go
+++ People.go
@@ -1,70 +1,114 @@
 package main
 
 import (
+  "database/sql"
 	"encoding/json"
 	"io/ioutil"
+
+  _ "github.com/lib/pq"
 )
 
 var ()
 
 type Database struct {
-	People map[string]Person
+  dbClient                     *sql.DB
+  log            *logrus.Logger
 }
 type Person struct {
 	UID     string
 	canAdd  bool
-	isFancy bool
-	isCheap bool
+	drinkFancy bool
 }
 
-func CreateDatabase() (database Database, err error) {
-	database.People = make(map[string]Person)
+func CreateDatabase(logger *logrus.Logger) (database Database, err error) {
+  database.log = logger
+  database.dbClient, err = sql.Open(server, "host=pgsql port=5432 user=postgress password=Thunder@01 dbname=postgres sslmode=disable")
+	if err != nil {
+		return
+	}
+	err = database.dbClient.Ping()
+	if err != nil {
+		return
+	}
+	database.log.Info("Connected to Database")
 	return
 }
 func (db *Database) hasUID(UID string) (Person Person, ok bool) {
-	Person, ok = db.People[UID]
+	err, DataMap := GetPersonData(UID)
+  if err != nil{
+    return
+  }
+  ok = true
+  //TODO COPY PERSON INFO here
 	return
 }
-func (db *Database) AddAdmin(UID string) (err error) {
-	db.AddPerson(UID, true, false, false)
-	return
-}
-func (db *Database) AddBarner(UID string) (err error) {
-	db.AddPerson(UID, false, true, false)
-	return
-}
-func (db *Database) AddFriend(UID string) (err error) {
-	db.AddPerson(UID, false, false, true)
-	return
-}
-func (db *Database) Commit() (err error) {
-	dataToWrite, err := json.Marshal(db.People)
-	if err != nil {
-		log.Error("Marshal Error: %v")
-		return
-	}
-	err = ioutil.WriteFile("data/people.json", dataToWrite, 0666)
-	if err != nil {
-		log.Error("Error Commiting: %v")
-	}
-	return
-}
-func (db *Database) Load() (err error) {
-	dataToLoad, err := ioutil.ReadFile("data/people.json")
-	if err != nil {
-		log.Error("Read file: %v")
-	}
-	err = json.Unmarshal(dataToLoad, &db)
-	if err != nil {
-		log.Error("Error Loading: %v")
-	}
-	return
-}
-func (db *Database) AddPerson(UID string, isAdmin, isBarner, isFriend bool) (err error) {
+func (db *Database) AddPerson(UID string, isAdmin, isBarner bool) (err error) {
 	_, exists := db.People[UID]
 	if UID != "" && !exists {
-		toAdd := Person{UID, false, false, true}
+		toAdd := Person{UID, isAdmin, isBarner}
 		db.People[UID] = toAdd
 	}
 	return
+}
+func (db *Database) GetPersonData(UID string) (err error, DataMap map[string]interface{}) {
+	err, _, DataMapArray := db.RunQueryRows(fmt.Sprintf("SELECT * from \"users\" WHERE \"users\".\"UID\" = '%v'", UID))
+	if err != nil {
+		return
+	}
+	db.log.Tracef("DataMapArray: %+v", DataMapArray)
+	//TODO if this is greater than 0 database has wrong information len(NodeMapArray)
+	if DataMapArray == nil {
+		err = errors.New("DataMapArray is null")
+		return
+	}
+	DataMap = DataMapArray[0]
+	if DataMap == nil {
+		err = errors.New("User is not in database")
+		return
+	}
+/*
+	SerialNumber, ok := NodeMap["SerialNumber"].(string)
+	if ok == false {
+		err = errors.New("SerialNumber not of type string")
+		return
+	}
+  */
+	return
+}
+func (db *Database) RunQueryRows(queryString string) (error, int, []map[string]interface{}) {
+	db.log.Trace("Query: ", queryString)
+	var dataMapArray []map[string]interface{}
+	rows, err := db.dbClient.Query(queryString)
+	if err != nil {
+		return err, 0, dataMapArray
+	}
+	defer rows.Close()
+	cols, err := rows.Columns()
+	if err != nil {
+		return err, 0, dataMapArray
+	}
+
+	for rows.Next() {
+		rowMap := make(map[string]interface{})
+		// Create a slice of interface{}'s to represent each column,
+		// and a second slice to contain pointers to each item in the columns slice.
+		columns := make([]interface{}, len(cols))
+		columnPointers := make([]interface{}, len(cols))
+		for i, _ := range columns {
+			columnPointers[i] = &columns[i]
+		}
+		// Scan the result into the column pointers...
+		if err := rows.Scan(columnPointers...); err != nil {
+			return err, 0, dataMapArray
+		}
+		// Create our map, and retrieve the value for each column from the pointers slice,
+		// storing it in the map with the name of the column as the key.
+		for i, colName := range cols {
+			val := columnPointers[i].(*interface{})
+			rowMap[colName] = *val
+		}
+		// Outputs: map[columnName:value columnName2:value2 columnName3:value3 ...]
+		dataMapArray = append(dataMapArray, rowMap)
+	}
+	return nil, len(dataMapArray), dataMapArray
 }
